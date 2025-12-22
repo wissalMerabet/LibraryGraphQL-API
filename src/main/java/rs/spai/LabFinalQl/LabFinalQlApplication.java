a1ce8170654a6e4098ddeeccb76ec17a071ab437
wissal <wissalmerabet5@gmail.com>
2025-12-22 12:56:01 +0100

    update env varbl

--- src/main/java/rs/spai/LabFinalQl/LabFinalQlApplication.java
+++ src/main/java/rs/spai/LabFinalQl/LabFinalQlApplication.java
@@ -3,11 +3,13 @@ package rs.spai.LabFinalQl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LabFinalQlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabFinalQlApplication.class, args);
+
 	}
 
 }
